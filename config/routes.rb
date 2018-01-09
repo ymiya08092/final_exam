92ed4fac28b5a3d0c2f44fc8e6ae84fbe866f837
Yuki Miyahara <y_miya@nttpc.co.jp>
2018-01-09 08:25:36 +0000

    init

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'tops#index'
+   resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
