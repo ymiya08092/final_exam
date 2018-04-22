3312fef02b866858b273e5787f7b8701a494251f
Yuki Miyahara <y_miya@nttpc.co.jp>
2018-04-22 06:29:03 +0000

    init

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -72,7 +72,7 @@ class PicturesController < ApplicationController
   def require_login
     unless logged_in?
       flash[:error] = "You must be logged in to access this section"
-      redirect_to new_user_path # halts request cycle
+      redirect_to sessions_new_path # halts request cycle
     end
   end
  
