d817757ea5d06e2b9405418bafa5b81a8df76962
Yuki Miyahara <y_miya@nttpc.co.jp>
2018-05-18 09:50:51 +0000

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,:confirmable
   validates :name,  presence: true, length: { maximum: 30 }, on: :create
   validates :email, presence: true, length: { maximum: 255 }, on: :create,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
